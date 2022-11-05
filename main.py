7ce6397e899ed7be51603467c3f3c3412e4d4abf
soolaar <aruzhanakhmetova6@gmail.com>
2022-11-05 17:12:09 +0600

    libraries

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print('hi')
-print('Aru')
-print('I am Madina')
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+from flask_bcrypt import Bcrypt
+from datetime import datetime
